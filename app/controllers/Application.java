6eec30d40fcba920471f652d5d01c9c7fc2ac8e9
113414A0093L4 <113414A0093L4@NESH201702200.nsl.ad.nec.co.jp>
2018-12-13 16:41:54 +0900

    deleted tests

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -41,7 +41,7 @@ public class Application extends Controller {
         if (isSetup()) {
             if (code == null) {
                 // start oauth
-                final String url = "https://login.salesforce.com/services/oauth2/authorize?response_type=code" +
+                final String url = "https://test.salesforce.com/services/oauth2/authorize?response_type=code" +
                         "&client_id=" + force.consumerKey() +
                         "&redirect_uri=" + oauthCallbackUrl(request());
                 return CompletableFuture.completedFuture(redirect(url));
@@ -89,7 +89,7 @@ public class Application extends Controller {
         }
 
         CompletionStage<AuthInfo> getToken(String code, String redirectUrl) {
-            final CompletionStage<WSResponse> responsePromise = ws.url("https://login.salesforce.com/services/oauth2/token")
+            final CompletionStage<WSResponse> responsePromise = ws.url("https://test.salesforce.com/services/oauth2/token")
                     .addQueryParameter("grant_type", "authorization_code")
                     .addQueryParameter("code", code)
                     .addQueryParameter("client_id", consumerKey())
@@ -116,7 +116,7 @@ public class Application extends Controller {
             public String Name;
             public String Type;
             public String Industry;
-            public String Rating;
+            public String BillingCountry;
         }
 
         @JsonIgnoreProperties(ignoreUnknown = true)
@@ -140,9 +140,9 @@ public class Application extends Controller {
         }
 
         CompletionStage<List<Account>> getAccounts(AuthInfo authInfo) {
-            CompletionStage<WSResponse> responsePromise = ws.url(authInfo.instanceUrl + "/services/data/v34.0/query/")
+            CompletionStage<WSResponse> responsePromise = ws.url(authInfo.instanceUrl + "/services/data/v44.0/query/")
                     .addHeader("Authorization", "Bearer " + authInfo.accessToken)
-                    .addQueryParameter("q", "SELECT Id, Name, Type, Industry, Rating FROM Account")
+                    .addQueryParameter("q", "SELECT Id, Name, Type, Industry, BillingCountry FROM Account")
                     .get();
 
             return responsePromise.thenCompose(response -> {
